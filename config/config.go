4fe0d797c82907b6878df9a5e916a3515814edfa
Akecel <axel@bailfacile.fr>
2023-03-29 21:49:18 +0200

    🔧 Update config

--- config/config.go
+++ config/config.go
@@ -7,9 +7,9 @@ import (
 )
 
 func InitConfig() {
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("$HOME/bin/")
-	viper.AddConfigPath("./tests/")
+	viper.AddConfigPath(".") // Local config
+	viper.AddConfigPath("$HOME/bin/") // Binary config
+	viper.AddConfigPath("../") // Test config
 	viper.SetConfigName("gabbro")
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
