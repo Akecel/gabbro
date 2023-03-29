1f199322f542f6e236c8478286edb545adeb9fac
Akecel <axel@bailfacile.fr>
2023-03-29 20:53:41 +0200

    🚨 Test

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
+        "github.com/spf13/cobra"
 )
 
 var (
-	RootCmd = &cobra.Command{
-		Use:   "gabbro",
-		Short: "Simple CLI to interact with IGDB API",
-		Long: `Gabbro is a simple CLI written in Go allowing interaction with the IGDB API. It is possible to search for information about specific video game and many other things.`,
-	}
+        RootCmd = &cobra.Command{
+                Use:   "gabbro",
+                Short: "Simple CLI to interact with IGDB API",
+                Long:  `Gabbro is a simple CLI written in Go allowing interaction with the IGDB API. It is possible to search for information about specific video game and many other things.`,
+        }
 )
 
-func Execute() (error) {
-	return RootCmd.Execute()
+func Execute() error {
+        return RootCmd.Execute()
 }
