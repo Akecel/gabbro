90d8203e8e1b19a3725e8f366c549929693cd064
Akecel <axel@bailfacile.fr>
2023-04-05 13:29:40 +0200

    🚀 Deploy scripts

---
+++ scripts/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+package=$1
+
+if [[ -z "$package" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+
+package_split=(${package//\// })
+package_len=${#package_split[@]}
+package_name=${package_split[${package_len} - 1]} 
+	
+platforms=("windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
+
+echo "==> Building..."
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name=$package_name'-'$GOOS'-'$GOARCH
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi	
+
+echo "Build:" ${GOOS}/${GOARCH}
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o ./bin/$output_name $package
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
+
+echo
+echo "==> Results:"
+ls -1 bin/
